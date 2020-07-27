d6a9f91deea9d7c2fc675f1729513f9ad08033c5
Paweł Rogowski <p.rogowski3074@student.po.edu.pl>
2020-07-27 12:03:45 +0200

    Model

---
+++ Modul/Models/ZolozeniaDlaOferty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modul.Models
+{
+    public class ZolozeniaDlaOferty
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string PokazFormularz { get; set; }
+        [Required]
+        public string CzyRolnik { get; set; }
+        [Required]
+        public int ProgPodatkowy { get; set; }
+        [Required]
+        public Wojewodztwo Wojewodztwo { get; set; }
+        [Required]
+        public int MiesieczneRachunki { get; set; }
+        public string ZnajomoscRocznychRachunkowiZuzywanaEnergia { get; set; }
+        [Required]
+        public string ImieiNazwiskoKlienta { get; set; }
+        public string Paneltechniczny { get; set; }
+        public string WycenaNaZyczenie { get; set; }
+
+
+    }
+    public enum Wojewodztwo
+    {
+        dolnośląskie, kujawsko_pomorskie, lubelskie, lubuskie, łódzkie, małopolskie, mazowieckie,
+        opolskie, podkarpackie, podlaskie, pomorskie, śląskie, świętokrzyskie, warmińsko_mazurskie, 
+        wielkopolskie, zachodniopomorskie
+    }
+}
