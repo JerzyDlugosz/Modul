ad3a07dd90b66d37ec3d8f66fa7b9b1bd7c10ced
Mateusz Twardowski <m.twardowski@student.po.edu.pl>
2020-07-28 11:09:19 +0200

    Register/Login page + migration

---
+++ Modul/Data/Migrations/20200727135443_ZalozenieToDatabaseChange.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Modul.Data.Migrations
+{
+    public partial class ZalozenieToDatabaseChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CzyNaVat",
+                table: "ZolozeniaDlaOferty",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CzyNaVat",
+                table: "ZolozeniaDlaOferty");
+        }
+    }
+}
